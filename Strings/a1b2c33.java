b86a30ee55f5badc2dc15e54d3bff03c56ec6ca4
ARAVINTH <aravinths911@gmail.com>
2023-06-13 17:00:44 +0530

    13/6/23 -2

---
+++ Strings/a1b2c33.java
@@ -0,0 +1,33 @@
+// input : a1b2c10
+// output : abbcccccccccc
+
+import java.util.*;
+public class a1b2c33 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String nums = "";
+        char letter = ' ';
+        for(char c: str.toCharArray()){
+            if(!(c>='0' && c<='9')){
+                if(nums!=""){
+                    int n = Integer.parseInt(nums);
+                    for(int i=0;i<n;i++){
+                        System.out.print(letter);
+                    }
+               
+                }
+            nums = "";
+            letter = c;    // for last iteration keep track of character
+
+            }
+            else{
+                nums = nums+c;
+            }
+        }
+        int n = Integer.parseInt(nums);
+        for(int i=0;i<n;i++){
+            System.out.print(letter);
+        }
+    }
+}
