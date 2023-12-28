2c5fb5a21684dd10f9e1d7687f53b37999250461
ARAVINTH <aravinths911@gmail.com>
2023-12-28 12:02:50 +0530

    28-12-2023  strings

--- test2.java
+++ test2.java
@@ -2,39 +2,37 @@ import java.util.Arrays;
 import java.util.Stack;
 
 class Solution {
-    public static int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> stack = new Stack<>();
 
-        for (int asteroid : asteroids) {
-            if (asteroid > 0) {
-                stack.push(asteroid);
-                System.out.println("postive ast : " + stack);
-            } else {
-                while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < Math.abs(asteroid)) {
-                    stack.pop(); // Destroy the asteroid moving to the right
-                    System.out.println("While loop : " + stack);
-                }
+    public static String reverse(String str) {
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        return rev;
+    }
 
-                if (stack.isEmpty() || stack.peek() < 0) {
-                    stack.push(asteroid); // No collision or surviving asteroid moving to the left
-                    System.out.println("negative peek and ast peek : " + stack);
-                } else if (stack.peek() == Math.abs(asteroid)) {
-                    stack.pop(); // Both asteroids annihilate each other
-                    System.out.println("annihilate each other : " + stack);
-                }
+    public static String reverseStr(String s, int k) {
+
+        String res = "";
+        int i = 0;
+        int rev = 0;
+        while (i <= s.length() - k) {
+            if (rev != k) {
+                res += reverse(s.substring(i, i + k));
+                i = i + k;
+            }
+            if (rev == k) {
+                res += s.charAt(i);
+                i += 1;
             }
         }
 
-        int[] result = new int[stack.size()];
-        for (int i = stack.size() - 1; i >= 0; i--) {
-            result[i] = stack.pop();
-        }
-
-        return result;
+        return res;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 5, -2, 8, -6, 3, -10 };
-        System.out.println(Arrays.toString(asteroidCollision(arr)));
+        String s = "abcd";
+        int k = 2;
+        System.out.println(reverseStr(s, k));
     }
 }
